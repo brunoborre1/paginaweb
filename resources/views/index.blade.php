59259c60cde1e97293de69ca59918ab4666a21c6
brunoborre1 <brunoborrello@gmail.com>
2020-10-14 22:02:16 -0300

    comienzo a subir archivos

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+@extends('layout')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+
+<nav class="barra">
+    <ul>
+        <li><a href="index">Inicio</a></li>
+        <li><a href="contacto">Contacto</a></li>
+        <li><a href="nosotros">Nosotros</a></li>
+    </ul>
+
+</nav>
+    
+</body>
+</html>
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,38 @@
+
+<style>
+
+
+ul{
+    list-style-type: none;
+    margin: 0px;
+    padding: 0px;
+    background: #008B8B;
+    overflow: hidden;
+}
+
+li{
+    float: left;
+}
+
+li a{
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 16px 16px;
+    text-decoration: none;
+}
+
+li a:visited{
+    color: #AFEEEE;
+}
+
+li a:hover{
+    background-color: #B0C4DE;
+    color: #A52A2A;
+}
+
+</style>
+
+
+
+
