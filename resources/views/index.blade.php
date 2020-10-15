d679714c83b9737e6ed13ff3673f3335b138995b
brunoborre1 <brunoborrello@gmail.com>
2020-10-14 22:17:54 -0300

    guardo cambios

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
+@extends('layout')
 <!DOCTYPE html>
 <html lang="en">
-@extends('layout')
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
